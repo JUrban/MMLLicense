ffff7ee441b5ae3a3c4769234b2ec3c3c1253b6a
Josef Urban <urban@urban.(none)>
2010-09-22 17:59:21 +0200

    added an optional file argument

--- git-authors.pl
+++ git-authors.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/env perl
 
 # run this in a git directory to get all contributors
+# with an argument, it gets contributors for that file
 
-/(\d{4}).*?_(.+)/&&\$a{$2}->{$1}for`git log --pretty=format:%aD_%an`;
+my $file = ($#ARGV == 0)? shift(@ARGV) : '';
+
+/(\d{4}).*?_(.+)/&&\$a{$2}->{$1}for`git log --pretty=format:%aD_%an $file`;
 print"Copyright (C) ".join(", ",sort keys %{$a{$_}})." $_.\n"for sort keys %a
